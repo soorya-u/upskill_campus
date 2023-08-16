e68f2ddc3e67f87670a6ca234682d7cd705ae595
Soorya U <sooryau7@gmail.com>
2023-08-16 17:57:55 +0530

    Adding Decrypt Function - Cryptography

--- Cryptography/EncryptAndDecrypt.py
+++ Cryptography/EncryptAndDecrypt.py
@@ -20,3 +20,8 @@ def Encrypt(string,key):
     if key is not None:
         string_bytes = bytes(string,'utf-8')
         return Fernet(key).encrypt(string_bytes)
+    
+def Decrypt(string_byte,key):
+    if key is not None:
+        string = Fernet(key).decrypt(string_byte).decode()
+        return string
