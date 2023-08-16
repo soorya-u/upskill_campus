42bf2550372b5940dc53d8764c8905edc9a7ce50
Hamsa <hamsashettycs@gmail.com>
2023-08-16 14:27:44 +0530

    update password

--- Database/internship/Upskill-internship/User/leveldb/_db_manager.py
+++ Database/internship/Upskill-internship/User/leveldb/_db_manager.py
@@ -9,3 +9,14 @@ def store_password(website,url,username,email,password,description):
     myCur.execute(f'''insert into UserDataBase values ("{website}","{url}","{username}","{email}","{password}","{description}");''')
     conn.commit()
     conn.close()
+
+def update_password(website,new_password):
+    conn = connect_database()  
+    myCur = conn.cursor()
+
+    sqlQuery = f'''update UserDataBase set Password = "{new_password}" where Website = "{website}"'''
+
+    myCur.execute(sqlQuery)
+    conn.commit()
+    conn.close()
+
