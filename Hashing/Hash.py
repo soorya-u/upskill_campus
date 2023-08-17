ecfbb696b00fb1a08ad9bbae4dcda547cd0c241e
saanvi-MJ <saanvijasmine@gmail.com>
2023-08-17 14:14:46 +0530

    Adding Verify Hashing - Hashing

--- Hashing/Hash.py
+++ Hashing/Hash.py
@@ -3,3 +3,10 @@ import hashlib
 def creatingHash(password):
     password_hash=hashlib.sha512(password.encode()).hexdigest()
     return password_hash
+
+def verifyingHash(password,stored_password_hash):
+
+    password_hash=hashlib.sha512(password.encode()).hexdigest()
+    if(password_hash==stored_password_hash):
+        return True
+    return False
