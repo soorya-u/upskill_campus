aa0f8c29c27eab4b4d7c9c8d4c53af284ef447c6
Soorya U <sooryau7@gmail.com>
2023-08-27 09:33:00 +0530

    Adding Generate Password Function - Hash

--- Hash.py
+++ Hash.py
@@ -1,4 +1,5 @@
 import hashlib
+import random
 
 def creatingHash(password):
     password_hash=hashlib.sha512(password.encode()).hexdigest()
@@ -12,4 +13,22 @@ def verifyingHash(password,stored_password_hash):
     return False
 
 def generatePassword():
-    pass
+    password=[]
+    pass_len = random.randint(8,31)
+    num="0123456789"
+    low_alp="abcdefghijklmnopqrstuvwxyz"
+    upp_alp="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    spec_char=r"!@#$%^&*_-+=:;<>?/.,"
+
+    for i in range(pass_len):
+        r = random.randint(1,4)
+        if r == 1:
+            password.append(num[random.randint(0,len(num)-1)])
+        elif r == 2:
+            password.append(low_alp[random.randint(0,len(low_alp)-1)])
+        elif r == 3:
+            password.append(upp_alp[random.randint(0,len(upp_alp)-1)])
+        else:
+            password.append(spec_char[random.randint(0,len(spec_char)-1)])
+
+    return "".join(password[::-1])
